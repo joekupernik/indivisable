83b705969ee1c6c6e420e091b895423ef1b7a7f0
Joe Kupernik <joekupernik@gmail.com>
2023-01-19 13:09:53 -0800

    copy over all components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./header";
+import Footer from "./footer";
+import Content from './content';
+import Jumbotron from './jumbotron';
+import ReviewCard from './reviews';
+import ContactForm from './contactForm';
+
+
+
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Jumbotron/>
+      <Content/>
+      <ReviewCard/>
+      <ContactForm/>
+      <Footer/>
     </div>
+
   );
 }
 
