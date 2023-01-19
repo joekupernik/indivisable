ad88f0b7ea0eb6e72bd1a4a10ce5c9ddde7b3b7e
Joe Kupernik <joekupernik@gmail.com>
2023-01-19 14:46:44 -0800

    add photos and setup layout for reviews

--- src/reviews.js
+++ src/reviews.js
@@ -5,6 +5,7 @@ import CardContent from "@mui/material/CardContent";
 import Paper  from "@mui/material/Paper";
 //import Button from "@mui/material/Button";
 import Box  from "@mui/material/Box";
+import CardMedia from "@mui/material/CardMedia";
 
 export default function ReviewCard() {
   return (
@@ -12,7 +13,13 @@ export default function ReviewCard() {
     <Paper elevation="24" sx={{mt: 2, mb: 2}}>
       <Card variant="outlined" sx={{mt: 2, mb: 2}}>
           <CardContent sx={{display: "flex", justifyContent: "center"}}>
-      </CardContent>
+      
+      <CardMedia
+        component="img"
+        sx={{ width: '45%' }}
+        image="./images/coorsfield.jpg"
+        alt="landscape"
+      />
       <Typography  
         component="body"
         align="center"
@@ -21,26 +28,15 @@ export default function ReviewCard() {
         fontSize="1.5rem"
         >
        Joseph is a highly motivated and persistent engineer with a growing portfolio of excellent work. He is a capable engineer, good communicator and hard worker. He would be a great addition to any team looking for an engineer who is dedicated to producing quality work, will work hard and deliver.
-      </Typography>
-      
-      
-      <Typography 
-       component="body"
-       align="center"
-       color="text.primary"
-       fontSize={"1.7rem"}
-       >
-        <p> - Nancy Felix </p>
-        <a href="https://www.linkedin.com/in/nancyfelix123/"> Sr Leader, Software Development - Amazon Devices </a>
-      </Typography>
-      <CardContent sx={{display: "flex", justifyContent: "center"}}>
+      </Typography>      
       </CardContent>
       </Card>
       </Paper>
       <Paper elevation="24" sx={{mt: 2, mb: 2}}>
       <Card variant="outlined" sx={{mt: 2, mb: 2}}>
           <CardContent sx={{display: "flex", justifyContent: "center"}}>
-      </CardContent>
+      
+      
       <Typography  
         component="body"
         align="center"
@@ -48,22 +44,40 @@ export default function ReviewCard() {
         padding="1%"
         fontSize="1.5rem"
         >
-       Joe is a dedicated and persistent front end engineer. He is well versed in react and JavaScript as well as CSS and the frameworks Material UI and bootstrap. We have worked on several projects together and Joe's communication was clear and easy to understand. I would recommend Joe for any position where clear and effective communication and technical skills are required.
-      </Typography>
-      
-      <Typography 
-       component="body"
-       align="center"
-       color="text.primary"
-       fontSize="1.7rem"
-       >
-        <p> - Ben Kupernik </p>
-        <a href="https://www.linkedin.com/in/nancyfelix123/"> Scientific Software Engineer - Geomega </a>
-      </Typography>
-      <CardContent sx={{display: "flex", justifyContent: "center"}}>
+       Joseph is a highly motivated and persistent engineer with a growing portfolio of excellent work. He is a capable engineer, good communicator and hard worker. He would be a great addition to any team looking for an engineer who is dedicated to producing quality work, will work hard and deliver.
+      </Typography>  
+      <CardMedia
+        component="img"
+        sx={{ width: '45%' }}
+        image="./images/farmers.jpg"
+        alt="landscape"
+      />    
       </CardContent>
       </Card>
       </Paper>
+      <Paper elevation="24" sx={{mt: 2, mb: 2}}>
+      <Card variant="outlined" sx={{mt: 2, mb: 2}}>
+          <CardContent sx={{display: "flex", justifyContent: "center"}}>
+      
+      <CardMedia
+        component="img"
+        sx={{ width: '45%' }}
+        image="./images/smallbuisness.jpg"
+        alt="landscape"
+      />
+      <Typography  
+        component="body"
+        align="center"
+        color="text.primary"
+        padding="1%"
+        fontSize="1.5rem"
+        >
+       Joseph is a highly motivated and persistent engineer with a growing portfolio of excellent work. He is a capable engineer, good communicator and hard worker. He would be a great addition to any team looking for an engineer who is dedicated to producing quality work, will work hard and deliver.
+      </Typography>      
+      </CardContent>
+      </Card>
+      </Paper>
+      
       </Box>
   );
 }
