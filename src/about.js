81143e8cd947a581539e2c641d3276fe73416b6d
Joe Kupernik <joekupernik@gmail.com>
2023-01-20 14:06:09 -0800

    add json posts file and functionaility in about pg

--- src/about.js
+++ src/about.js
@@ -1,16 +1,34 @@
-import Card from '@mui/material/Card';
-import Paper from '@mui/material/Paper';
+import { Link }  from "react-router-dom";
+import Markdown from "markdown-to-jsx";
+import postlist from "./posts.json";
 
 
-function About() {
-    return (
-       <Paper>
-        <Card>
-           hello
-        </Card>
-       </Paper> 
-      
-    );
+
+const PostList = () => {
+  const excerptList = postlist.map(post => {
+      return post.content.split(" ").slice(0, 20).join(" ") + "..."
+  })
+  return (
+      <div className="postlist">
+          <h1 className="title">All Posts</h1>
+          {postlist.length && 
+              postlist.map((post, i) => {
+                  return (
+                      <div key={i} className="post-card">
+                           <div className="img-container">
+                              {post.thumbnail && <img className="thumbnail" width={80} src={post.thumbnail} alt=""/> }
+                              <h2 className="post-title"><Link className="links" to={`/post/${post.id}`}>{post.title}</Link></h2>
+                          </div>
+                          <small>Published on {post.date} by {post.author}</small>
+                          <hr/>
+                          <Markdown source={excerptList[i]} escapeHtml={false} />
+                          <small><Link className="links" to={`/post/${post.id}`}>Read more</Link></small>
+                      </div>
+                  )
+              })
+          }
+      </div>
+  )
 }
 
-export default About;
+export default PostList
