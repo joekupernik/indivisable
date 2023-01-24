59b3e115e665991210b5d4eaebc6170196fcd071
Joe Kupernik <joekupernik@gmail.com>
2023-01-24 10:33:04 -0800

    added dynamic rendering  individual posts to post

--- src/pages/post.js
+++ src/pages/post.js
@@ -1,20 +1,41 @@
 import React from "react";
-import { useParams } from "react-router-dom";
-//import Markdown from "react-markdown";
+import { useParams, Redirect } from "react-router-dom";
+import Markdown from "react-markdown";
 import Paper  from "@mui/material/Paper";
-//import postlist from "../posts.json";
+import postlist from "../posts.json";
 
 
 const Post = (props) => {
     const {postId} = useParams();
 
     console.log(postId);
+    const validId = parseInt(postId)
+    if (!validId) {
+        return <Redirect to="/404" />
+    }
+    const fetchedPost = {}
+    let postExists = false
+    postlist.forEach((post, i) => {
+        if (validId === post.id) {
+            fetchedPost.title = post.title ? post.title : "No title given"
+            fetchedPost.date = post.date ? post.date : "No date given"
+            fetchedPost.author = post.author ? post.author : "No author given"
+            fetchedPost.content = post.content ? post.content : "No content given"
+            postExists = true
+        }
+    })
+    if (postExists === false) {
+        return <Redirect to="/404" />
+    }
     return (
         
         <Paper>
             <div className="post">
                 <h2>post id {postId}</h2>
-                
+                <h2>{fetchedPost.title}</h2>
+                <small>Published on {fetchedPost.date} by {fetchedPost.author}</small>
+                <hr/>
+                <Markdown children={fetchedPost.content}  />
             </div>
         </Paper>
     )
